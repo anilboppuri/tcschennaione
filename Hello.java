6e1b6654b24c99e5aa1434a3252d74b8ad4d313f
anilboppuri <anil4java2@gmail.com>
2024-02-24 18:48:17 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello{
+public static void main(String args[]){
+  System.out.println(10+20);
+  System.out.println(100-20);
+  System.out.println(100*200);
+  System.out.println(100/20);
+}
+}
