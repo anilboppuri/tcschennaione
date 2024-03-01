f4506c9da91caee37d7499af00580125b464b6d5
anilboppuri <anilboppuri@users.noreply.github.com>
2024-03-01 12:18:50 +0530

    Update Hello.java
    Added line 9 print statement

--- Hello.java
+++ Hello.java
@@ -6,6 +6,6 @@ public static void main(String args[]){
   System.out.println(100/20);
 
    System.out.println("1st changes added to source ");
-
+   System.out.println("2nd  changes added to source ");
 }
 }
